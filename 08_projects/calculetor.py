cabbbbfcdea22d2ce1da78029fce740009e70819
M TAUFIQUL ISLAM ROUDRO <s2010928167@ru.ac.bd>
2026-07-31 20:19:16 +0600

    Add basic calculator functionality in calculetor.py

---
+++ 08_projects/calculetor.py
@@ -0,0 +1,7 @@
+a = float(input("First number: "))
+b = float(input("Second number: "))
+
+print("Addition =", a+b)
+print("Subtraction =", a-b)
+print("Multiplication =", a*b)
+print("Division =", a/b)
